683fe0fa571193a5716e7bcec45f48b0ad58ff37
einkinder <einkinder@mac.com>
2016-05-01 12:53:46 -0400

    Setup for Global Database - Information used by all

--- config/database.php
+++ config/database.php
@@ -66,13 +66,13 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
+        'pgsql_global' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('GLOBAL_DB_HOST', 'localhost'),
+            'port' => env('GLOBAL_DB_PORT', '5432'),
+            'database' => env('GLOBAL_DB_DATABASE', 'forge'),
+            'username' => env('GLOBAL_DB_USERNAME', 'forge'),
+            'password' => env('GLOBAL_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
